80480e7e524467a8ef3484a54db2e1518ceff964
FVS <feliven@gmail.com>
2025-09-30 13:39:04 -0300

    criada classe Conta

--- src/components/extrato-component.ts
+++ src/components/extrato-component.ts
@@ -7,16 +7,14 @@ const elementoRegistroTransacoesExtrato: HTMLElement = document.querySelector(".
 
 renderizarExtrato();
 function renderizarExtrato(): void {
-    const gruposTransacoes: GrupoTransacao[] = Conta.getGruposTransacoes();
-    elementoRegistroTransacoesExtrato.innerHTML = "";
-    let htmlRegistroTransacoes: string = "";
+  const gruposTransacoes: GrupoTransacao[] = Conta.getGruposTransacoes();
+  elementoRegistroTransacoesExtrato.innerHTML = "";
+  let htmlRegistroTransacoes: string = "";
 
-    for (let grupoTransacao of gruposTransacoes)
-    {
-        let htmlTransacaoItem: string = "";
-        for (let transacao of grupoTransacao.transacoes)
-        {
-            htmlTransacaoItem += `
+  for (let grupoTransacao of gruposTransacoes) {
+    let htmlTransacaoItem: string = "";
+    for (let transacao of grupoTransacao.transacoes) {
+      htmlTransacaoItem += `
                 <div class="transacao-item">
                     <div class="transacao-info">
                         <span class="tipo">${transacao.tipoTransacao}</span>
@@ -25,27 +23,27 @@ function renderizarExtrato(): void {
                     <time class="data">${formatarData(transacao.data, FormatoData.DIA_MES)}</time>
                 </div>
             `;
-        }
+    }
 
-        htmlRegistroTransacoes += `
+    htmlRegistroTransacoes += `
             <div class="transacoes-group">
                 <strong class="mes-group">${grupoTransacao.label}</strong>
                 ${htmlTransacaoItem}
             </div>
         `;
-    }
+  }
 
-    if (htmlRegistroTransacoes === "") {
-        htmlRegistroTransacoes = "<div>Não há transações registradas.</div>";
-    }
+  if (htmlRegistroTransacoes === "") {
+    htmlRegistroTransacoes = "<div>Não há transações registradas.</div>";
+  }
 
-    elementoRegistroTransacoesExtrato.innerHTML = htmlRegistroTransacoes;
+  elementoRegistroTransacoesExtrato.innerHTML = htmlRegistroTransacoes;
 }
 
 const ExtratoComponent = {
-    atualizar(): void {
-        renderizarExtrato();
-    }
-}
+  atualizar(): void {
+    renderizarExtrato();
+  },
+};
 
 export default ExtratoComponent;
--- src/components/saldo-component.ts
+++ src/components/saldo-component.ts
@@ -6,20 +6,20 @@ const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLEleme
 const elementoDataAcesso = document.querySelector(".block-saldo time") as HTMLElement;
 
 if (elementoDataAcesso != null) {
-    elementoDataAcesso.textContent = formatarData(Conta.getDataAcesso(), FormatoData.DIA_SEMANA_DIA_MES_ANO);
+  elementoDataAcesso.textContent = formatarData(Conta.getDataAcesso(), FormatoData.DIA_SEMANA_DIA_MES_ANO);
 }
 
 renderizarSaldo();
 function renderizarSaldo(): void {
-    if (elementoSaldo != null) {
-        elementoSaldo.textContent = formatarMoeda(Conta.getSaldo());
-    }
+  if (elementoSaldo != null) {
+    elementoSaldo.textContent = formatarMoeda(Conta.getSaldo());
+  }
 }
 
 const SaldoComponent = {
-    atualizar() {
-        renderizarSaldo();
-    }
-}
+  atualizar() {
+    renderizarSaldo();
+  },
+};
 
 export default SaldoComponent;
